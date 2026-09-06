4a818587a1d1fdbb38ab88234f10ad4cead0040a
Mazen Medhat <134063525+MaZeNN113@users.noreply.github.com>
2026-09-07 00:38:42 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,4 @@
-
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+android { namespace = "com.mazen.ezik"; compileSdk = 35
+ defaultConfig { applicationId = "com.mazen.ezik"; minSdk = 29; targetSdk = 35; versionCode = 1; versionName = "0.1.0" }
+}
